b9e97467318c6764788e1b6aa862497ae38db766
JoanChirinos <jchirinos@stuy.edu>
2018-02-09 00:09:21 -0500

    Final test and graph

--- MFDriver.java
+++ MFDriver.java
@@ -44,9 +44,9 @@ public class MFDriver {
   //gets average run time for worst case of a matrix of a set size. Runs
   //search on the worst case scenario 100 times
   public static long[] getRuntimes(int sizeOfMatrix) {
-    long[] timeStore = new long[1000];
+    long[] timeStore = new long[1050];
     int[][] matrix = popMatrix(sizeOfMatrix);
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 1050; i++) {
       timeStore[i] = timer(matrix, 2 * sizeOfMatrix - 2);
     }
     return timeStore;
@@ -55,13 +55,17 @@ public class MFDriver {
   //Tests runtimes for every 50 array sizes between [10k, 20k] and writes to
   //data.csv. Also prints time taken, in seconds, afterwards
   public static void main(String[] args) {
+    //"warm-up"
+    for (int i = 0; i < 1000; i++) {
+      int[] xd = new int[5000];
+    }
     long start = System.nanoTime();
     String toWrite = "";
     for (int i = 10000; i <= 20000; i += 50) {
       long[] times = getRuntimes(i);
       System.out.println(i);
-      for (long x : times) {
-        toWrite += i + "," + x + "\n";
+      for (int x = 50; x < 1050; x++) {
+        toWrite += i + "," + times[x] + "\n";
       }
     }
     FileRW.write(toWrite, "data.csv");
